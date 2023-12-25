87176b269714b7ac0eac9ec343d221023abb9a4d
iamnmk <numaankhan@Numaans-MacBook-Air.local>
2023-12-25 21:51:04 +0300

    done

--- Codepulse.API/Program.cs
+++ Codepulse.API/Program.cs
@@ -1,4 +1,7 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Codepulse.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(Options =>
+{
+    Options.UseSqlServer(builder.Configuration.GetConnectionString("CodePulseConnectionString"));
+
+});
 
 var app = builder.Build();
 
