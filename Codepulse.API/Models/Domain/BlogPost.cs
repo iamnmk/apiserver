7242c8060716216338e1ee421d22fc094e5b8023
iamnmk <numaankhan@Numaans-MacBook-Air.local>
2023-12-25 17:03:25 +0300

    done

---
+++ Codepulse.API/Models/Domain/BlogPost.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Codepulse.API.Models.Domain
+{
+	public class BlogPost
+	{
+		public Guid Id { get; set; }
+
+		public String Title { get; set; }
+
+        public String ShortDescription{ get; set; }
+
+        public String Content { get; set; }
+
+        public String FeatureImageurl { get; set; }
+
+        public String UrlHandle { get; set; }
+
+        public DateTime PublishedDate { get; set; }
+
+        public String Author { get; set; }
+
+        public bool IsVisible { get; set; }
+
+    
+
+    }
+}
+
